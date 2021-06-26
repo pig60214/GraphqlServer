fb0b69c4b0adf236c9b11beb3a48a7a3380badf1
Yuxiu <no>
2021-06-26 20:13:24 +0800

    Add Repositories

---
+++ src/repositories/Post.js
@@ -0,0 +1,20 @@
+let posts = [
+    { id: "P1", from: '2021/06/05', to: '2021/06/07', title: 'Implement Set Up Project', processStatus: "ToDo" },
+    { id: "P2", from: '2021/06/13', to: '2021/06/15', title: 'Implement Post Query', processStatus: "ToDo" },
+];
+
+function getPosts(postsQueryInput) {
+    const { from, to } = postsQueryInput;
+    const fromOfQuery = new Date(from);
+    const toOfQuery = new Date(to);
+    return posts.filter(post => {
+        const fromOfPost = new Date(post.from);
+        const toOfPost = new Date(post.to);
+        const outOfRange = toOfPost < fromOfQuery || fromOfPost > toOfQuery;
+        return !outOfRange;
+      })
+}
+
+module.exports = {
+    getPosts
+}
--- src/resolvers/queries/Post.resolvers.js
+++ src/resolvers/queries/Post.resolvers.js
@@ -1,20 +1,9 @@
-const posts = [
-  { id: "P1", from: '2021/06/05', to: '2021/06/07', title: 'Implement Set Up Project', processStatus: "ToDo" },
-  { id: "P2", from: '2021/06/13', to: '2021/06/15', title: 'Implement Post Query', processStatus: "ToDo" },
-];
+const { getPosts } = require('../../repositories/Post')
 
 module.exports = {
   Query: {
     posts: (_, { postsQueryInput }) => {
-      const { from, to } = postsQueryInput;
-      const fromOfQuery = new Date(from);
-      const toOfQuery = new Date(to);
-      return posts.filter(post => {
-        const fromOfPost = new Date(post.from);
-        const toOfPost = new Date(post.to);
-        const outOfRange = toOfPost < fromOfQuery || fromOfPost > toOfQuery;
-        return !outOfRange;
-      });
+      return getPosts(postsQueryInput);
     },
   },
 };
