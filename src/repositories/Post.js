854cde2e92e961bc4d786942a33f32da3b233222
Yuxiu <no>
2021-07-31 00:16:40 +0800

    Upload Images To Imgur

--- src/repositories/Post.js
+++ src/repositories/Post.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 let posts = [
     { id: "P1", from: '2021/06/05', to: '2021/06/07', title: 'Implement Set Up Project', processStatus: "ToDo", photos: [{ path: 'https://i.imgur.com/O6usdNx_d.webp?maxwidth=760&fidelity=grand', caption: 'Tree House 2' }] },
     { id: "P2", from: '2021/06/13', to: '2021/06/15', title: 'Implement Post Query', processStatus: "ToDo", photos: [{ path: 'https://i.imgur.com/O6usdNx_d.webp?maxwidth=760&fidelity=grand', caption: 'Tree House 2' }] },
@@ -16,7 +18,28 @@ function getPosts(postsQueryInput) {
 }
 
 function addPost(addPostInput){
-    var { title } = addPostInput;
+    var { title, images } = addPostInput;
+    const config = {
+      url: 'https://api.imgur.com/3/image',
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer beaf5d7bf18bed387f377c2d0668a9a88b4cd10e',
+      },
+    };
+
+    images.forEach(image => {
+      config.data = {
+        image,
+        title: title,
+        album: 'uDUHmuu',
+      };
+
+      axios(config)
+        .then(res => console.log(res))
+        .catch(e => console.log(e));
+    });
+
     var id = `P${posts.length}`;
     var post = { id: id, title: title, from: '2021/06/05', to: '2021/06/07' }
     posts.push(post);
