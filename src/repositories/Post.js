ae374d4a3bccb6e35090b03626aa43a7392df94f
Yuxiu <no>
2021-08-02 10:56:27 +0800

    Extract Convert Function And Fix Time Zone Bug

--- src/repositories/Post.js
+++ src/repositories/Post.js
@@ -2,10 +2,17 @@ const axios = require('axios');
 const { pool, poolConnect } = require('./connection');
 const sql=require('mssql');
 
-let posts = [
-    { id: "P1", from: '2021/06/05', to: '2021/06/07', title: 'Implement Set Up Project', processStatus: "ToDo", photos: [{ path: 'https://i.imgur.com/O6usdNx_d.webp?maxwidth=760&fidelity=grand', caption: 'Tree House 2' }] },
-    { id: "P2", from: '2021/06/13', to: '2021/06/15', title: 'Implement Post Query', processStatus: "ToDo", photos: [{ path: 'https://i.imgur.com/O6usdNx_d.webp?maxwidth=760&fidelity=grand', caption: 'Tree House 2' }] },
-];
+function toGraphqlType(post) {
+    const fromDate = post.FromDate;
+    const toDate = post.ToDate;
+
+    return {
+        id: post.PostId,
+        title: post.Title,
+        from: fromDate.toISOString().replace('Z', ''),
+        to: toDate.toISOString().replace('Z', ''),
+    }
+}
 
 async function getPosts(postsQueryInput) {
     const { from, to } = postsQueryInput;
@@ -15,14 +22,7 @@ async function getPosts(postsQueryInput) {
             .input('fromDate', sql.DateTime, from)
             .input('toDate', sql.DateTime, to)
             .execute('GetPosts');
-        const posts = result.recordset.map(post => {
-            return {
-                id: post.PostId,
-                title: post.Title,
-                from: new Date(post.FromDate).toISOString(),
-                to: new Date(post.ToDate).toISOString(),
-            }
-        });
+        const posts = result.recordset.map(post => toGraphqlType(post));
         return posts;
     } catch (err) {
         console.error('SQL error', err);
@@ -60,14 +60,7 @@ async function addPost(addPostInput){
             .input('fromDate', sql.DateTime, from)
             .input('toDate', sql.DateTime, to)
             .execute('AddPost');
-        const posts = result.recordset.map(post => {
-            return {
-                id: post.PostId,
-                title: post.Title,
-                from: new Date(post.FromDate).toISOString(),
-                to: new Date(post.ToDate).toISOString(),
-            }
-        });
+        const posts = result.recordset.map(post => toGraphqlType(post));
         return posts[0];
     } catch (err) {
         console.error('SQL error', err);
