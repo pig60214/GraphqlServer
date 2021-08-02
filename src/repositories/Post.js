73b19362a0ae01bbddb2e0641b32fed116952529
Yuxiu <no>
2021-08-02 21:32:56 +0800

    Get Photo From DB

--- src/repositories/Post.js
+++ src/repositories/Post.js
@@ -1,7 +1,7 @@
 const { pool, poolConnect } = require('./connection');
 const sql=require('mssql');
 
-function toGraphqlType(post) {
+function toPostGraphqlType(post) {
     const fromDate = post.FromDate;
     const toDate = post.ToDate;
 
@@ -13,6 +13,13 @@ function toGraphqlType(post) {
     }
 }
 
+function toPhotoGraphqlType(photo) {
+  return {
+    path: photo.ImgurLink,
+    caption: photo.Caption,
+  }
+}
+
 async function getPosts(postsQueryInput) {
     const { from, to } = postsQueryInput;
     await poolConnect; // ensures that the pool has been created
@@ -21,7 +28,11 @@ async function getPosts(postsQueryInput) {
             .input('fromDate', sql.DateTime, from)
             .input('toDate', sql.DateTime, to)
             .execute('GetPosts');
-        const posts = result.recordset.map(post => toGraphqlType(post));
+        const posts = result.recordset.map(post => toPostGraphqlType(post));
+        const photos = result.recordsets[1];
+        posts.forEach(post => {
+          post.photos = photos.filter(photo => photo.PostId === post.id).map(photo => toPhotoGraphqlType(photo));
+        });
         return posts;
     } catch (err) {
         console.error('SQL error', err);
@@ -39,7 +50,7 @@ async function addPost(addPostInput){
             .input('fromDate', sql.DateTime, from)
             .input('toDate', sql.DateTime, to)
             .execute('AddPost');
-        const posts = result.recordset.map(post => toGraphqlType(post));
+        const posts = result.recordset.map(post => toPostGraphqlType(post));
         return posts[0];
     } catch (err) {
         console.error('SQL error', err);
