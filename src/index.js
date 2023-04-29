b955eac872fa1254c819b783cdf7c129d3dc9bf1
YX <pig60214@gmail.com>
2023-04-30 01:40:17 +0800

    Migrate To Apollo Server
    te

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-const { ApolloServer } = require('apollo-server-express');
-const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 
-const app = express();
-app.use(express.json({limit : '2100000kb'}));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
-
-apolloServer.applyMiddleware({ app });
-
-if(process.env.NODE_ENV === 'development') {
-  app.listen({ port: 4000 },
-    () => console.log(`GraphQL Server ready at http://localhost:4000${apolloServer.graphqlPath}`));  // eslint-disable-line
-} else {
-  app.listen(() => console.log(`GraphQL Server ready at http://{{domainName}}${apolloServer.graphqlPath}`));  // eslint-disable-line
+async function start() {
+  const url = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+  console.log(`🚀  Server ready at: ${url.url}`);
 }
+
+start();
