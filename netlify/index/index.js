9032eab1de70280372aef2216c4ca42a962f75ec
YX <pig60214@gmail.com>
2023-04-30 01:41:31 +0800

    Add Lambda Apollo Server

---
+++ netlify/index/index.js
@@ -0,0 +1,18 @@
+const { ApolloServer } = require('apollo-server-lambda');
+const typeDefs = require('../../src/schema');
+const resolvers = require('../../src/resolvers');
+
+const getHandler = (event, context) => {
+  const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      debug: true,
+  });
+  const graphqlHandler = server.createHandler();
+  if (!event.requestContext) {
+      event.requestContext = context;
+  }
+  return graphqlHandler(event, context);
+}
+
+exports.handler = getHandler;
